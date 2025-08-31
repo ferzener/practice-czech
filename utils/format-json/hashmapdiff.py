3f15106e91557e28c8cd2f398abcf68c6bb7a68f
Ferzener <ferzener+github@proton.me>
2025-08-31 23:39:45 +0100

    Update hashmapdiff.py

--- utils/format-json/hashmapdiff.py
+++ utils/format-json/hashmapdiff.py
@@ -1,14 +1,20 @@
+import json
+import sys
+
 file1_path = input("file 1 path -> ")
 file2_path = input("file 2 path -> ")
 
-with open("file1_path", "r") as file1:
-  file1_lines = file1.remove("{").remove("}").split(",")
+# Load both files as JSON objects (dicts)
+with open(file1_path, "r", encoding="utf-8") as f1:
+    obj1 = json.load(f1)  # keys to remove
 
-with open("file2_path", "r") as file2:
-  file2_lines = file2.remove("{").remove("}").split(",")
+with open(file2_path, "r", encoding="utf-8") as f2:
+    obj2 = json.load(f2)  # source map
 
-for line in file2_lines:
-  if line in file1_lines:
-    file2_lines.remove(line)
+# Remove from obj2 any key that appears in obj1
+keys_to_remove = set(obj1.keys())
+result = {k: v for k, v in obj2.items() if k not in keys_to_remove}
 
-print("{" + ",".join(file2_lines) + "}")
+# Print valid JSON (preserve Unicode)
+json.dump(result, sys.stdout, ensure_ascii=False, indent=2, sort_keys=True)
+print()
