14dc9c9b351c5a5e5de0ecaa63ea81648d30a0cc
Ferzener <ferzener+github@proton.me>
2025-08-31 23:07:07 +0100

    Update main.py

--- utils/format-json/main.py
+++ utils/format-json/main.py
@@ -1,6 +1,6 @@
 import json
 
-words_file_name = input("Base file path")
+words_file_name = input("Base file path -> ")
 
 with open(words_file_name, "r") as words_file:
   words_file_lines = words_file.read().split("\n")
@@ -8,11 +8,12 @@ with open(words_file_name, "r") as words_file:
 words_hashmap = dict()
 
 for line in words_file_lines:
-  word = line.split('"')[1].split['"'][0]
-  raw_meanings = line.split("[")[1].split("]")[0]
+  word = line.split(":")[0]
+  print(word)
+  raw_meanings = line.split(":")[1].split(",")
   meanings = []
   for meaning in raw_meanings:
-    meanings.append(strip(meaning))
+    meanings.append(meaning.strip())
   words_hashmap[word] = meaning
 
 print(json.dumps(words_hashmap))
